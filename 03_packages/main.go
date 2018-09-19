0338ce44ce2c260dc8d0276fe49255264ecbfb86
Devin Herath <devdhera@gmail.com>
2018-09-19 20:10:28 +0530

    Using Packages

---
+++ 03_packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/DevDHera/go_starter/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(10.6))
+	fmt.Println(math.Ceil(10.6))
+	fmt.Println(math.Sqrt(25))
+	fmt.Println(strutil.Reverse("): siht ees ot nethgilnE eB"))
+}
